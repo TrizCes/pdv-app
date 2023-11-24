87e2ac996eec7305b6151e352661a1b9f71c3abc
Yara-Garcia <yaracoelho77@gmail.com>
2023-11-24 12:09:35 -0300

    endpoint de cadastro de usuario criado (faltam validações)

---
+++ src/controladores/usuarios.js
@@ -0,0 +1,33 @@
+const knex = require('../utilitarios/conexao');
+const { hash } = require('bcrypt');
+
+const cadastrarUsuario = async (req, res) => {
+    const { nome, email, senha } = req.body;
+
+    try {
+        const usuarioJaExiste = await knex('usuarios').where({ email }).first();
+
+        if (usuarioJaExiste) {
+            return res.status(400).json("E-mail já está cadastrado!");
+        }
+
+        const senhaCriptografada = await hash(senha, 10);
+
+        const novoUsuario = await knex('usuarios').insert({ nome, email, senha: senhaCriptografada }).returning('*');
+
+        if (!novoUsuario || novoUsuariosuario.length === 0) {
+            return res.status(400).json("O usuário não foi cadastrado.");
+        }
+
+        const { senha: _, ...dadosUsuario } = novoUsuario[0]
+
+        return res.status(200).json(dadosUsuario)
+
+    } catch (error) {
+        return res.status(400).json({ mensagem: 'Erro interno no servidor' });
+    }
+}
+
+module.exports = {
+    cadastrarUsuario
+}
