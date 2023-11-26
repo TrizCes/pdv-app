25bd52603082d3ca2eeb859635ad97d5827bae46
Flavia <139448717+flaviaros4@users.noreply.github.com>
2023-11-26 14:13:34 -0300

    Adição da rota detalhar usuário

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const listarCategorias = require('./controladores/categorias.js');
+const { detalharUsuario } = require('./controladores/usuarios.js');
+const verificarLogin = require('./intermediarios/autenticacao.js');
 
 const rotas = express();
 
 rotas.get('/categoria', listarCategorias);
+
+rotas.get('/usuario', verificarLogin, detalharUsuario)
 
 module.exports = rotas;
