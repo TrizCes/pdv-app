07e25a81cdc00519cdd3776376d9e3cf23a5bfdc
Beatriz Ceschini <biaceschini@gmail.com>
2023-11-27 15:16:51 -0300

    Validações de atualizar usuário;

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,62 +1,60 @@
 const knex = require('../utilitarios/conexao');
 const { hash } = require('bcrypt');
 
-
 const atualizarUsuario = async (req, res) => {
-    const { nome, email, senha } = req.body;
+  const { nome, email, senha } = req.body;
+  const { id } = req.usuario;
 
-    try {
-        const usuarioJaExiste = await knex('usuarios').where({ email }).first()
+  try {
+    const usuarioJaExiste = await knex('usuarios').where({ email }).first();
 
-        if (usuarioJaExiste) {
-            return res.status(400).json('E-mail já está cadastrado!')
-        }
-
-        const senhaCriptografada = await hash(senha, 10);
-
-        const usuarioAtualizado = await knex('usuarios')
-            .where({ id: req.usuario.id })
-            .update({ nome, email, senha: senhaCriptografada })
-
-        if (!usuarioAtualizado) {
-            return res.status(400).json("O usuario não foi atualizado");
-        }
-
-        return res.status(200).json('Usuario foi atualizado com sucesso.');
-
-    } catch (error) {
-        return res.status(400).json({ mensagem: 'Erro interno no servidor' });
+    if (usuarioJaExiste && usuarioJaExiste.id !== id) {
+      return res.status(400).json('E-mail já está cadastrado!');
     }
+
+    const senhaCriptografada = await hash(senha, 10);
+
+    const usuarioAtualizado = await knex('usuarios')
+      .where({ id: req.usuario.id })
+      .update({ nome, email, senha: senhaCriptografada });
+
+    if (!usuarioAtualizado) {
+      return res.status(400).json('O usuario não foi atualizado');
+    }
+
+    return res.status(200).json('Usuario foi atualizado com sucesso.');
+  } catch (error) {
+    return res.status(400).json({ mensagem: 'Erro interno no servidor' });
+  }
 };
 
 const cadastrarUsuario = async (req, res) => {
-    const { nome, email, senha } = req.body;
+  const { nome, email, senha } = req.body;
 
-    try {
-        const usuarioJaExiste = await knex('usuarios').where({ email }).first();
+  try {
+    const usuarioJaExiste = await knex('usuarios').where({ email }).first();
 
-        if (usuarioJaExiste) {
-            return res.status(400).json("E-mail já está cadastrado!");
-        }
-
-        const senhaCriptografada = await hash(senha, 10);
-
-        const novoUsuario = await knex('usuarios').insert({ nome, email, senha: senhaCriptografada }).returning('*');
-
-        if (!novoUsuario || novoUsuario.length === 0) {
-            return res.status(400).json("O usuário não foi cadastrado.");
-        }
-
-        const { senha: _, ...dadosUsuario } = novoUsuario[0]
-
-        return res.status(201).json(dadosUsuario)
-
-    } catch (error) {
-        return res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    if (usuarioJaExiste) {
+      return res.status(400).json('E-mail já está cadastrado!');
     }
-}
+
+    const senhaCriptografada = await hash(senha, 10);
+
+    const novoUsuario = await knex('usuarios').insert({ nome, email, senha: senhaCriptografada }).returning('*');
+
+    if (!novoUsuario || novoUsuario.length === 0) {
+      return res.status(400).json('O usuário não foi cadastrado.');
+    }
+
+    const { senha: _, ...dadosUsuario } = novoUsuario[0];
+
+    return res.status(201).json(dadosUsuario);
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno no servidor' });
+  }
+};
 
 module.exports = {
-    cadastrarUsuario,
-    atualizarUsuario
-}
+  cadastrarUsuario,
+  atualizarUsuario,
+};
