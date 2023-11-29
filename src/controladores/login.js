51f73c51fb2b06052762d35b0d96c709446fed65
Bianca <bibia.the.rox@gmail.com>
2023-11-29 15:00:18 -0300

    status 400

--- src/controladores/login.js
+++ src/controladores/login.js
@@ -7,7 +7,7 @@ const login = async (req, res) => {
     const { email, senha } = req.body;
 
     if (!email || !senha) {
-        return res.status(500).json({ mensagem: "Todos os campos são obrigatórios!" });
+        return res.status(400).json({ mensagem: "Todos os campos são obrigatórios!" });
     }
 
     try {
