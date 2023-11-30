60ead7dad25fef3819faff875e65609ea59875da
Yara-Garcia <yaracoelho77@gmail.com>
2023-11-30 17:03:29 -0300

    Inseridas no schemaProduto validações de numero inteiro e/ou positivo

--- src/validacoes/schemaProduto.js
+++ src/validacoes/schemaProduto.js
@@ -9,14 +9,19 @@ const schemaProduto = joi.object({
   quantidade_estoque: joi.number().integer().min(0).required().messages({
     'any.required': 'O campo quantidade_estoque é obrigatório',
     'number.base': 'É necessário informar a quantidade de estoque',
+    'number.integer': 'A quantidade do produto deve ser um número inteiro',
+    'number.min': 'A quantidade do produto deve ser maior ou igual a zero',
   }),
   valor: joi.number().integer().positive().required().messages({
     'any.required': 'O campo valor é obrigatório',
     'number.base': 'É necessário informar o valor do produto.',
+    'number.integer': 'O valor do produto deve ser um número inteiro',
+    'number.positive': 'O valor do produto deve ser um número positivo',
   }),
   categoria_id: joi.number().integer().required().messages({
     'any.required': 'O campo categoria é obrigatório',
     'number.base': 'É necessário informar a categoria do produto.',
+    'number.integer': 'A categoria do produto deve ser um número inteiro',
   }),
 });
 
